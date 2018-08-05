076b52e4835bee7219da44d5d7b9acb1f01380ce
Gurpreet <m19.gurpreet@gmail.com>
2018-08-04 18:29:36 -0700

    update

--- linkedLists/linkedList.c
+++ linkedLists/linkedList.c
@@ -1,43 +1,5 @@
 #include "linkedlist.h"
-
-void separateEvenAndOdd(struct node* head){
-  /*a->b->c->d->e->null
-   1  2  3  4  5
-   a->c->e->b->d->null output
-   odd nodes to the front, even nodes to the end while preseving relative order  */ 
-   
-   struct node* odd = head;
-   struct node* even = head->next;
-   struct node* evenH = even;
-
-   if(odd == NULL)
-       return;
-      
-   printf("%c\n",odd->data);
-   printf("%c\n",even->data); 
-
-   while(odd != NULL || even != NULL) {
-       odd->next = even->next;
-       if ( odd->next == NULL) {
-           printf("exiting now\n");
-           break ;
-       }
-       odd = odd->next;
-       printf("%c\n",odd->data);
-
-       even->next  = odd->next;
-       if ( even->next == NULL) {
-           printf("exiting now\n");
-           break ;
-       }
-       even = even->next;
-       printf("%c\n",even->data);
-   }  
-       
-   odd->next = evenH;   
-   printf("printing sep odd and even\n");  
-   printList(head); 
-}
+#include "separateEvenAndOddNodes.c"
 
 int main() {
    struct node* head = NULL;
